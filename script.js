3d32412a5c2ebd52748115e335b873af7371fcce
Alexandre Le Roy <alr@linux.com>
2021-04-11 17:35:49 +0200

    Update load-test script

--- script.js
+++ script.js
@@ -2,21 +2,35 @@ import http from 'k6/http';
 
 export let options = {
 	scenarios: {
-		endpoint: {
+		foo: {
 			executor: 'ramping-arrival-rate',
+			exec: 'foo',
 			startRate: 50,
-			timeUnit: '1s',
+			timeUnit: '1m',
 			preAllocatedVUs: 1,
-			maxVUs: 1000,
+			maxVUs: 100000,
 			stages: [
-				{ target: 100, duration: '3m' },
 				{ target: 1000, duration: '3m' },
 				{ target: 10000, duration: '3m' },
+				{ target: 100000, duration: '3m' },
 			],
+		},
+		bar: {
+			executor: 'constant-arrival-rate',
+			exec: 'bar',
+			rate: 10000,
+			duration: '5m',
+			timeUnit: '1s',
+			preAllocatedVUs: 1,
+			maxVUs: 100000,
 		},
 	},
 };
 
-export default function () {
-	http.get('http://localhost:8080/');
+export function foo() {
+	http.get('http://localhost:8080/foo');
+}
+
+export function bar() {
+	http.get('http://localhost:8080/bar');
 }
